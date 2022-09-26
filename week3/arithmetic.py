293d38da4225e1645a31ac210cf928d722c7b150
wangshaohuai <t108820013@ntut.org.tw>
2022-09-26 14:58:24 +0800

    add exercise arith.py

---
+++ week3/arithmetic.py
@@ -0,0 +1,15 @@
+i1 = eval(input())
+i2 = eval(input())
+i3 = input()
+if(i3 == "+"):
+    print(i1 + i2)
+elif(i3 == "-"):
+    print(i1 - i2)
+elif(i3 == "*"):
+    print(i1 * i2)
+elif(i3 == "/"):
+    print(i1/i2)
+elif(i3 == "%"):
+    print(i1 % i2)
+elif(i3 == "//"):
+    print(i1//i2)
