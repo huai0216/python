f2bbc1b333dc7dbcf1eca0fd092798804d243955
huai0216 <t108820013@ntut.org.tw>
2022-10-24 14:47:52 +0800

    creditcard.py finish

--- week6/creditcard.py
+++ week6/creditcard.py
@@ -1,12 +1,27 @@
 import math
 credit = eval(input("Enter a credit card number:"))
-num = []
 
-i=1
-for i in range(17):
-    ten = 10 ** i
-    num.append(credit % ten)   
-    credit = credit // ten
-    print(credit)
-    i += 1
-    #print(num)
+
+def isValid(number):
+    num = list(str(number))
+    evensum = 0
+    oddsum =  0
+    for i in range(0,16,2):
+        even = int(num[i]) * 2
+        if(even > 9) :
+            even10 = even //10
+            even1  = even % 10
+            even = even10 + even1
+            evensum = even + evensum
+        else :
+            evensum = even + evensum
+    for i in range(1, 17, 2):
+        odd = int(num[i])
+        oddsum = odd + oddsum
+    return oddsum + evensum
+
+if isValid(credit) %10 == 0:
+    result = "valid"
+else :
+    result = "invalid"
+print('The credit card {} is {}'.format(credit, result))
