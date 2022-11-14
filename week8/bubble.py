423c5b8db6fe26fb2a509d3511a97e61d14ee357
huai0216 <t108820013@ntut.org.tw>
2022-11-14 13:47:18 +0800

    revise bubble.py

--- week8/bubble.py
+++ week8/bubble.py
@@ -1,7 +1,7 @@
 bubble = []
 bubble = list(map(int,input("Enter numbers:").split()))
 n = len(bubble)
-for i in range(n-2):
+for i in range(n):
     for j in range(n-i-1):
         if bubble[j]>bubble[j+1]:
             bubble[j],bubble[j+1] = bubble[j+1],bubble[j]
