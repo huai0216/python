b54bc3d1376772e90b5c4135a4f5f492b0babd1a
huai0216 <t108820013@ntut.org.tw>
2022-11-28 18:26:23 +0800

    add GuessNumber.py

---
+++ week10/GuessNumber.py
@@ -0,0 +1,14 @@
+import random 
+number = random.randint(0,100)
+print(number)
+print("Guess a magic number between 0 to 100")
+guess = -1
+while guess != number :
+    guess = eval(input("Enter your guess: "))
+    if guess == number :
+        print("Yes, the number is",number)
+    elif guess > number :
+        print("Your guess is too high")
+    else :
+        print("Your guess is too low")
+
